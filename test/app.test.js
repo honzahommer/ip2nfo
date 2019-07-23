a128fce9aea4b423eb861a3cfb484730d4ce0a7d
Honza Hommer <honza@hommer.cz>
2019-07-23 21:24:35 +0200

    feat: add IPv6 support

--- test/app.test.js
+++ test/app.test.js
@@ -62,18 +62,6 @@ describe('ip2nfo/app', () => {
       });
   });
 
-  it('GET /::1', done => {
-    request(app)
-      .get('/::1')
-      .set('Accept', 'application/json')
-      .expect(417)
-      .end((err, res) => {
-        if (err) return done(err);
-        expect(res.body.message).to.not.be.undefined;
-        done();
-      });
-  });
-
   it('GET /1.1.1.1/foobar', done => {
     request(app)
       .get('/1.1.1.1/foobar')
@@ -100,6 +88,75 @@ describe('ip2nfo/app', () => {
   it('GET /8.8.8.8/distance', done => {
     request(app)
       .get('/8.8.8.8/distance')
+      .expect(200)
+      .end((err, res) => {
+        if (err) return done(err);
+        expect(res.text).to.not.be.equal('NaN');
+        done();
+      });
+  });
+
+  it('GET /2606:4700:4700::1111', done => {
+    request(app)
+      .get('/2606:4700:4700::1111')
+      .set('Accept', 'application/json')
+      .expect(200)
+      .end((err, res) => {
+        if (err) return done(err);
+        expect(res.body.hostname).to.be.equal('one.one.one.one');
+        done();
+      });
+  });
+
+  it('GET /2606:4700:4700::1111/hostname', done => {
+    request(app)
+      .get('/2606:4700:4700::1111/hostname')
+      .expect(200)
+      .end((err, res) => {
+        if (err) return done(err);
+        expect(res.text).to.be.equal('one.one.one.one');
+        done();
+      });
+  });
+
+  it('GET /fd17:4bfc:9c22:958c::1', done => {
+    request(app)
+      .get('/fd17:4bfc:9c22:958c::1')
+      .set('Accept', 'application/json')
+      .expect(200)
+      .end((err, res) => {
+        if (err) return done(err);
+        expect(res.body.bogon).to.be.true;
+        done();
+      });
+  });
+
+  it('GET /2606:4700:4700::1111/foobar', done => {
+    request(app)
+      .get('/2606:4700:4700::1111/foobar')
+      .set('Accept', 'application/json')
+      .expect(417)
+      .end((err, res) => {
+        if (err) return done(err);
+        expect(res.body.message).to.not.be.undefined;
+        done();
+      });
+  });
+
+  it('GET /2001:4860:4860::8888/distance/2620:fe::9', done => {
+    request(app)
+      .get('/2001:4860:4860::8888/distance/2620:fe::9')
+      .expect(200)
+      .end((err, res) => {
+        if (err) return done(err);
+        expect(Number(res.text)).to.be.approximately(52.386, 0.1);
+        done();
+      });
+  });
+
+  it('GET /2001:4860:4860::8888/distance', done => {
+    request(app)
+      .get('/2001:4860:4860::8888/distance')
       .expect(200)
       .end((err, res) => {
         if (err) return done(err);
--- test/ip2.test.js
+++ test/ip2.test.js
@@ -11,6 +11,15 @@ describe('ip2nfo/ip2', () => {
     });
   });
 
+  it('lookup 2606:4700:4700::1111', done => {
+    ip2.lookup('2606:4700:4700::1111').then(res => {
+      expect(res.hostname).to.be.equal('one.one.one.one');
+      done();
+    }).catch(err => {
+      return done(err);
+    });
+  });
+
   it('lookup 10.10.10.10', done => {
     ip2.lookup('10.10.10.10').then(res => {
       expect(res.latitude).to.be.equal(0);
@@ -21,9 +30,10 @@ describe('ip2nfo/ip2', () => {
     });
   });
 
-  it('lookup ::1', done => {
-    ip2.lookup('::1').then(res => {
-      expect(res.ip).to.be.undefined;
+  it('lookup fd17:4bfc:9c22:958c::1', done => {
+    ip2.lookup('fd17:4bfc:9c22:958c::1').then(res => {
+      expect(res.latitude).to.be.equal(0);
+      expect(res.longitude).to.be.equal(0);
       done();
     }).catch(err => {
       return done(err);
